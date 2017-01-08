14d7dade0ef7d5efabc929df8ebe99362a0455af
Taylor Rose <taylor.rose1@vivint.com>
2017-01-08 13:12:07 -0700

    no message

---
+++ js/cdn.js
@@ -0,0 +1,20 @@
+//Tool: http://www.accessify.com/tools-and-wizards/developer-tools/html-javascript-convertor/
+
+//-------****** Valentines Foot ******-------//
+var valentines_foot="";valentines_foot+="<h2>VALENTINES</h2>",valentines_foot+='<div class="foot">',valentines_foot+='<a class="fade" href="/valentines/downton-abbey-valentines/">',valentines_foot+='<div class="wrap"><img class="thumb" src="/images/valentines/downton-abbey-valentine.jpg" alt=""><span class="center">Downton Abbey</span></div>',valentines_foot+="</a>",valentines_foot+='<a class="fade" href="/valentines/lost-valentines/">',valentines_foot+='<div class="wrap"><img class="thumb" src="/images/valentines/lost-valentine.jpg" alt=""><span class="center">Lost</span></div>',valentines_foot+="</a>",valentines_foot+='<a class="fade" href="/valentines/harry-potter-valentines/">',valentines_foot+='<div class="wrap"><img class="thumb" src="/images/valentines/harry-potter-valentine.jpg" alt=""><span class="center">Harry Potter</span></div>',valentines_foot+="</a>",valentines_foot+='<a class="fade" href="/valentines/hunger-games-valentines">',valentines_foot+='<div class="wrap"><img class="thumb" src="/images/valentines/hunger-games-valentine.jpg" alt=""><span class="center">Hunger Games</span></div>',valentines_foot+="</a>",valentines_foot+="</div>";
+
+/*<h2>VALENTINES</h2>
+<div class="foot">
+<a class="fade" href="/valentines/downton-abbey-valentines/">
+<div class="wrap"><img class="thumb" src="/images/valentines/downton-abbey-valentine.jpg" alt=""><span class="center">Downton Abbey</span></div>
+</a>
+<a class="fade" href="/valentines/lost-valentines/">
+<div class="wrap"><img class="thumb" src="/images/valentines/lost-valentine.jpg" alt=""><span class="center">Lost</span></div>
+</a>
+<a class="fade" href="/valentines/harry-potter-valentines/">
+<div class="wrap"><img class="thumb" src="/images/valentines/harry-potter-valentine.jpg" alt=""><span class="center">Harry Potter</span></div>
+</a>
+<a class="fade" href="/valentines/hunger-games-valentines">
+<div class="wrap"><img class="thumb" src="/images/valentines/hunger-games-valentine.jpg" alt=""><span class="center">Hunger Games</span></div>
+</a>
+</div>*/
---
+++ js/valentines_foot.js
@@ -0,0 +1,18 @@
+var strVar="";
+strVar += "<h2>VALENTINES<\/h2>";
+strVar += "<div class=\"foot\">";
+strVar += "<a class=\"fade\" href=\"\/valentines\/downton-abbey-valentines\/\">";
+strVar += "<div class=\"wrap\"><img class=\"thumb\" src=\"\/images\/valentines\/downton-abbey-valentine.jpg\" alt=\"\"><span class=\"center\">Downton Abbey<\/span><\/div>";
+strVar += "<\/a>";
+strVar += "<a class=\"fade\" href=\"\/valentines\/lost-valentines\/\">";
+strVar += "<div class=\"wrap\"><img class=\"thumb\" src=\"\/images\/valentines\/lost-valentine.jpg\" alt=\"\"><span class=\"center\">Lost<\/span><\/div>";
+strVar += "<\/a>";
+strVar += "<a class=\"fade\" href=\"\/valentines\/harry-potter-valentines\/\">";
+strVar += "<div class=\"wrap\"><img class=\"thumb\" src=\"\/images\/valentines\/harry-potter-valentine.jpg\" alt=\"\"><span class=\"center\">Harry Potter<\/span><\/div>";
+strVar += "<\/a>";
+strVar += "<a class=\"fade\" href=\"\/valentines\/hunger-games-valentines\">";
+strVar += "<div class=\"wrap\"><img class=\"thumb\" src=\"\/images\/valentines\/hunger-games-valentine.jpg\" alt=\"\"><span class=\"center\">Hunger Games<\/span><\/div>";
+strVar += "<\/a>";
+strVar += "<\/div>";
+
+$(".valentines_foot").append(strVar);
