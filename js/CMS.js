5dbd0188ae7aa2e26fdeac21b65313362d15e505
Taylor Rose <taylor.rose1@vivint.com>
2017-01-12 20:23:05 -0700

    Fancy Box

--- js/CMS.js
+++ js/CMS.js
@@ -46,24 +46,7 @@ $(document).ready(function() {
 			$('.fancybox').fancybox();
 
 			$("#fancybox-manual-c").click(function() {
-				$.fancybox.open([
-					{
-						href : '1_b.jpg',
-						title : 'My title'
-					}, {
-						href : '2_b.jpg',
-						title : '2nd title'
-					}, {
-						href : '3_b.jpg'
-					}
-				], {
-					helpers : {
-						thumbs : {
-							width: 75,
-							height: 50
-						}
-					}
-				});
+				$.fancybox.open();
 			});
 
 		});
